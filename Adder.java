74bbdfeb3cfe3b78c05e536e005a8eec3206cb1f
sumitdhandar97 <sumitsdhandar@gmail.com>
2024-06-29 17:28:01 +0530

    new commit

--- Adder.java
+++ Adder.java
@@ -6,4 +6,7 @@ class TestOverloading1{
     public static void main(String[] args){
         System.out.println(Adder.add(11,11));
         System.out.println(Adder.add(11,11,11));
+        System.out.println("successful");
+
+
     }}
